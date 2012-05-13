13a065c92f6538e152b3f29ae4de895d6a0f67c4
Phil Austin <paustin@eos.ubc.ca>
2012-05-12 21:58:33 -0700

    add handles

--- thetae_test.m
+++ thetae_test.m
@@ -1,4 +1,4 @@
-function thetae_test
+function [temphandle,wvhandle]=thetae_test;
     numpress=50;
     numtheta=60;
     thetae0=linspace(280,340,numtheta);
@@ -8,29 +8,29 @@ function thetae_test
     out_wv=NaN([numpress,numtheta]);
     for i=1:numpress;
       for j=1:numtheta;
-        [i,j]
+        rowcol=[i,j]
         [Tcloud,wvCloud,wlCloud]=tinvert_thetae(thetae0(j),wT0, ...
                                                 press(i));
         out_temp(i,j)=Tcloud;
         out_wv(i,j)=wvCloud;
       end
     end
-    figure(1)
+    temphandle=figure(1);
     clf;
     contourf(thetae0,press*1.e-2,out_temp);
-    xlabel('theta_e (K)')
+    xlabel('thetae (K)')
     ylabel('pressure (hPa)')
-    title('Temperture (K) for w_T=10 g/kg')
+    title('Temperture (K) for wT=10 g/kg')
     colorbar;
-    print -dpng -r150 temp.png
-    figure(2)
-    clf
+    print -dpng -r150 temp.png;
+    wvhandle=figure(2);
+    clf;
     contourf(thetae0,press*1.e-2,out_wv*1.e3);
-    xlabel('theta_e (K)')
-    ylabel('pressure (hPa)')
-    title('vapor mixing ratio (g/kg) for w_T=10 g/kg')
+    xlabel('thetae (K)','fontsize',26);
+    ylabel('pressure (hPa)');
+    title('vapor mixing ratio (g/kg) for wT=10 g/kg');
     colorbar;
-    print -dpng -r150 wv.png
+    print -dpng -r150 wv.png;
 end
 
 
